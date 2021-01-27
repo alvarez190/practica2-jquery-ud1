b0387d9c13c298cb039c1812a6ebfbc146bbfc7b
stalin alvarez <stalinrap_123@hotmail.com>
2021-01-27 16:05:31 +0100

    [ADD]practica 2-3 de jQuery

---
+++ scripts/index.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+
+    /****** Style: ul#selected-play ***** */
+    $('#selected-plays>li').addClass("horizontal");
+    $('#selected-plays li ul').addClass("sub-level");
+    $('#selected-plays li ul li a[href^=mailto]').addClass("mailto");
+    $('#selected-plays li ul li a[href$=pdf]').addClass("pdflink");
+    $('#selected-plays a[href*=http]').addClass("henrylink");
+    /*************end************************ */
+
+    /**************style: table */
+    $('table:first tr:even').addClass("alt");
+    $('table:last tr:even').addClass("alt");
+    /**************end*************** */
+
+    /*******Estilos a todo lo relacionado con 'henry' */
+    $("table tr td:contains('Henry')").addClass("highlight");
+
+    /*********** Practica 3 - jQuery**************** */
+    // css en columna dos
+    $("table tr td:contains('Henry')").next().addClass("highlight");
+});
